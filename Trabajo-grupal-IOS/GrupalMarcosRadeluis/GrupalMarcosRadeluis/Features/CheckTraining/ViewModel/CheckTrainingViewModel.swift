e58ec87653312ac7bc7ace5f89dfcead7e302102
Radeluis Sanchez <rschz.dev@gmailcom>
2025-04-12 21:57:20 +0200

    Check training con servicio coredata

--- Trabajo-grupal-IOS/GrupalMarcosRadeluis/GrupalMarcosRadeluis/Features/CheckTraining/ViewModel/CheckTrainingViewModel.swift
+++ Trabajo-grupal-IOS/GrupalMarcosRadeluis/GrupalMarcosRadeluis/Features/CheckTraining/ViewModel/CheckTrainingViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct CheckTrainingViewModel {
 
@@ -17,6 +18,20 @@ struct CheckTrainingViewModel {
     init(idTraining: UUID) {
         self.idTraining = idTraining
         self.training = localPersistence.getTrainingByID(id: idTraining)
+    }
+    
+    func getRouteCoordinates() -> [CLLocationCoordinate2D]? {
+        guard let routeData = training?.route?.data(using: .utf8),
+              let coordinatesArray = try? JSONSerialization.jsonObject(with: routeData, options: []) as? [[String: Double]] else {
+            return nil
+        }
+        
+        return coordinatesArray.compactMap { dict in
+            if let lat = dict["latitude"], let lon = dict["longitude"] {
+                return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            }
+            return nil
+        }
     }
 }
 
